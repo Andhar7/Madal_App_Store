90ea20cbc0a8b42913674d53825791ff6847a288
Andhar7 <boxingprofi@web.de>
2026-05-08 16:15:25 +0200

    Removed — GeometryReader not needed Moved to .background(Color("Unity").ignoresSafeArea()) ScrollView(.vertical) — always scrollable .frame(maxHeight: .infinity) on VStack - removed!

--- Madal/Views/Helper/MenuView.swift
+++ Madal/Views/Helper/MenuView.swift
@@ -8,47 +8,43 @@
 import SwiftUI
 
 struct MenuView: View {
-    
+
     @State var isNavigationBarHidden = true
-    
+
     var body: some View {
-        
-        GeometryReader { geo in
-            
-            ScrollView(UIScreen.main.bounds.height < 750 ? .vertical : .init(), showsIndicators: false) {
-                
-                VStack(alignment: .leading, spacing: 18) {
-                    
-                    ForEach(MenuItems, id:\.self) { item in
-                        
-                        NavigationLink(destination: CategoryView(category: item.category)) {
-                            
-                            HStack(spacing: 33) {
-                                
-                                Image(systemName: item.image)
-                                    .resizable()
-                                    .renderingMode(.template)
-                                    .frame(width: 15, height: 15)
-                                    .foregroundColor(.white)
-                                
-                                Text(item.category)
-                                 .font(.system(size: 18, weight: .regular, design: .serif))
-                                 .kerning(2.4)
-                                 .foregroundColor(.white)
-                            }
+
+        ScrollView(.vertical, showsIndicators: false) {
+
+            VStack(alignment: .leading, spacing: 18) {
+
+                ForEach(MenuItems, id:\.self) { item in
+
+                    NavigationLink(destination: CategoryView(category: item.category)) {
+
+                        HStack(spacing: 33) {
+
+                            Image(systemName: item.image)
+                                .resizable()
+                                .renderingMode(.template)
+                                .frame(width: 15, height: 15)
+                                .foregroundColor(.white)
+
+                            Text(item.category)
+                             .font(.system(size: 18, weight: .regular, design: .serif))
+                             .kerning(2.4)
+                             .foregroundColor(.white)
                         }
-                        .transition(.move(edge: .trailing))
-                        
-                        Divider()
-                            .background()
                     }
+                    .transition(.move(edge: .trailing))
+
+                    Divider()
+                        .background()
                 }
-                .padding()
-                .frame( maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-                .background(Color("Unity"))
             }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
         }
-        .ignoresSafeArea()
+        .background(Color("Unity").ignoresSafeArea())
         .onAppear(perform: {
             isNavigationBarHidden = true
         })
